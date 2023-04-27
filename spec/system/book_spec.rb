8e2fbe4a0e6e99c855e71515350f38dd4babc9dd
hiroyuki.akiho <hiroyuki.akiho@appirits.com>
2023-04-27 13:17:14 +0900

    request-spec

--- spec/system/book_spec.rb
+++ spec/system/book_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe "books", type: :system do
         visit "/books" # /booksへHTTPメソッドGETでアクセス
         expect(page).to have_text("Books") # 表示されたページにBookという文字があることを確認
     end
+
+    it 'POST /books' do
+        visit "books/new"
+        fill_in 'Title', with: 'RubyBook' # Title入力欄に"RubyBook"を入力する
+        fill_in 'Author', with: 'matz' # Author入力欄に"matz"を入力する
+        click_button 'Create Book' # Create Bookボタンを押す
+
+        expect(page).to have_text("Book was successfully created.")
+        expect(page).to have_text("Title: RubyBook")
+        expect(page).to have_text("Author: matz")
+    end
 end
